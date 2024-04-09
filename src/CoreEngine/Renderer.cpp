7162321e15c7fc1475120b6a716880b5c9afbe05
fekox <facusantos123@gmail.com>
2024-04-09 11:53:10 -0300

    Change the color of the back ground

--- src/CoreEngine/Renderer.cpp
+++ src/CoreEngine/Renderer.cpp
@@ -42,7 +42,7 @@ namespace renderer
 
 	void Renderer::StartDraw()
 	{
-		GLCall(glClearColor(0.1f, 0.3f, 0.3f, 1.0f));
+		GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
