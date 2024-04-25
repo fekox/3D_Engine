2526fb073be80f7f690b2858695226be8f25929f
fekox <facusantos123@gmail.com>
2024-04-24 22:26:04 -0300

    Added 3 classes for different types of lights

---
+++ src/Lights/DirectionalLight.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Tools/DLL-Exp/dll-Exp-Inp.h"
+#include "..\libs\glm\include\glm.hpp"
+
+namespace directionalLight
+{
+	class EXPORT DirectionalLight
+	{
+		public:
+
+		enum DirectionalLightType
+		{
+			Desert,
+			Factory,
+			Horror,
+			BiochemicalLab
+		};
+
+		glm::vec3 direction;
+
+		glm::vec3 ambient;
+		glm::vec3 diffuse;
+		glm::vec3 specular;
+
+		DirectionalLight();
+		DirectionalLight(glm::vec3 direction, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular);
+		DirectionalLight(DirectionalLightType directionalLightType);
+		
+		void SetDirection(glm::vec3 newDirection);
+		void SetAmbient(glm::vec3 newAmbient);
+		void SetDiffuse(glm::vec3 newDiffuse);
+		void SetSpecular(glm::vec3 newSpecular);
+		~DirectionalLight();
+	};
+}
+
--- src/Lights/Light.cpp
+++ src/Lights/Light.cpp
@@ -1,4 +1,4 @@
-#include "Light.h"
+#include "Lights/Light.h"
 
 namespace light
 {
