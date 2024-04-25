5e7103fd212ebabf63a9c1f92c859ccbfecb4246
fekox <facusantos123@gmail.com>
2024-04-24 21:47:54 -0300

    Reapply "Create Light Cube.Shader and three classes for the lights"
    This reverts commit 27ea3ea4b5ed2aa2e85a6d5478b870d61f390c8c.

---
+++ src/Lights/PointLight.cpp
@@ -0,0 +1,118 @@
+#include "PointLight.h"
+
+namespace pointLight
+{
+	PointLight::PointLight(glm::vec3 position, glm::vec3 newAmbient, glm::vec3 newDiffuse, glm::vec3 newSpecular)
+	{
+		lightPos = position;
+		ambient = newAmbient;
+		diffuse = newDiffuse;
+		specular = newSpecular;
+
+		constant = 1.0f;
+		linear = 0.09f;
+		quadratic = 0.032f;
+	}
+
+	PointLight::PointLight(glm::vec3 position, glm::vec3 newAmbient, glm::vec3 newDiffuse, glm::vec3 newSpecular, float newConstant, float newLinear, float newQuadratic)
+	{
+		lightPos = position;
+		ambient = newAmbient;
+		diffuse = newDiffuse;
+		specular = newSpecular;
+
+		constant = newConstant;
+		linear = newLinear;
+		quadratic = newQuadratic;
+	}
+
+	PointLight::PointLight(PointLightType pointLightType, glm::vec3 position, glm::vec3 newAmbient, glm::vec3 newDiffuse, glm::vec3 newSpecular)
+	{
+		switch (pointLightType)
+		{
+			case pointLight::PointLight::Desert:
+
+				lightPos = position;
+				ambient = newAmbient;
+				diffuse = newDiffuse;
+				specular = newSpecular;
+
+				constant = 1.0f;
+				linear = 0.09;
+				quadratic = 0.032;
+
+			break;
+
+			case pointLight::PointLight::Factory:
+
+				lightPos = position;
+				ambient = newAmbient;
+				diffuse = newDiffuse;
+				specular = newSpecular;
+
+				constant = 1.0f;
+				linear = 0.09;
+				quadratic = 0.032;
+
+			break;
+
+			case pointLight::PointLight::Horror:
+
+				lightPos = position;
+				ambient = newAmbient;
+				diffuse = newDiffuse;
+				specular = newSpecular;
+
+				constant = 1.0f;
+				linear = 0.14;
+				quadratic = 0.07;
+
+			break;
+
+			case pointLight::PointLight::BiochemicalLab:
+
+				lightPos = position;
+				ambient = newAmbient;
+				diffuse = newDiffuse;
+				specular = newSpecular;
+
+				constant = 1.0f;
+				linear = 0.14;
+				quadratic = 0.07;
+
+			break;
+		}
+	}
+
+	void PointLight::SetPosition(glm::vec3 newPosition)
+	{
+		this->lightPos = newPosition;
+	}
+
+	void PointLight::SetLightColor(glm::vec3 newLightColor)
+	{
+		this->ambient = newLightColor * 0.1f;
+		this->diffuse = newLightColor;
+		this->specular = newLightColor;
+	}
+
+	void PointLight::SetConstant(float newConstant)
+	{
+		this->constant = newConstant;
+	}
+
+	void PointLight::SetLinear(float newLinear)
+	{
+		this->linear = newLinear;
+	}
+
+	void PointLight::SetQuadratic(float newQuadratic)
+	{
+		this->quadratic = newQuadratic;
+	}
+
+	PointLight::~PointLight()
+	{
+	
+	}
+}
